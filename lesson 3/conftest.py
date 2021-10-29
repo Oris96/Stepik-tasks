80b9037f49d39d0577d3fb0fddb0455feb903b45
m.zotov <Vb[fbkjrn,2021>
2021-10-29 15:00:08 +0300

    prepared to 4th lesson

--- lesson 3/conftest.py
+++ lesson 3/conftest.py
@@ -2,9 +2,17 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+# CHROME
+# options = Options()
+# options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+# browser = webdriver.Chrome(options=options)
+# FIREFOX
+# fp = webdriver.FirefoxProfile()
+# fp.set_preference("intl.accept_languages", user_language)
+# browser = webdriver.Firefox(firefox_profile=fp)
 
 def pytest_addoption(parser):
-	parser.addoption('--language', action='store', default='ru',
+	parser.addoption('--language', action='store', default='en',
                      help="Choose language")
 
 @pytest.fixture
